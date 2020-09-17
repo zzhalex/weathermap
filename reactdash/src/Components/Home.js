69cf61ae0e9bb80f57fb266b386b6bca39164705
zzhalex <zzh001xx@hotmail.com>
2020-09-17 00:27:11 -0700

    add query

--- reactdash/src/Components/Home.js
+++ reactdash/src/Components/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
 
 function Home() {
-  return <p>Home</p>;
+  const inputEl = useRef(null);
+  const cityQuery = () => {
+    let city = inputEl.current.value;
+    axios
+      .get("/weather", {
+        params: {
+          city: city,
+        },
+      })
+      .then(function (res) {
+        console.log(res);
+      });
+  };
+  return (
+    <div className="searchBar">
+      <div className="search-input-container">
+        <input
+          className="search-input"
+          ref={inputEl}
+          type="search"
+          placeholder="Search..."
+        />
+        <span className="material-icons" onClick={cityQuery}>
+          search
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ router.get("/", function (req, res) {
   res.sendFile("index.html");
 });
 
-async function getCurWeather() {
+async function getCurWeather(city) {
   let url = "http://api.openweathermap.org/data/2.5/weather";
-  let city = "vancouver";
   let appid = process.env.weatherApi;
+  console.log(city);
   try {
     const response = await axios.get(url, {
       params: {
@@ -17,15 +17,18 @@ async function getCurWeather() {
         appid: appid,
       },
     });
-    console.log(response);
+    //console.log(response);
     return response.data;
   } catch (error) {
-    console.error(error);
+    throw error;
+    //console.error(error);
   }
 }
 
 router.get("/weather", async function (req, res, next) {
-  let data = await getCurWeather();
+  console.log("api weather");
+  //console.log(req.query);
+  let data = await getCurWeather(req.query.city);
   res.send(data);
 });
 module.exports = router;
